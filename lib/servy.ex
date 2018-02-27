b85b0441888cba4cab9002b7ce478df259d7f507
Mailan Reiser <mailan.reiser@gmail.com>
2018-02-26 21:34:10 -0800

    Lesson 1-10

---
+++ lib/servy.ex
@@ -0,0 +1,7 @@
+defmodule Servy do
+  def hello(name) do
+    "Chao, #{name}!"
+  end
+end
+
+#IO.puts Servy.hello("Elixir")
---
+++ lib/servy/handler.ex
@@ -0,0 +1,302 @@
+require Logger
+
+defmodule Servy.Handler do
+
+  @moduledoc "Handles HTTP requests."
+
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  @doc "Transforms the request into a response."
+  def handle(request) do
+    request
+    |> parse
+    |> rewrite_path
+    |> log
+    |> route
+    |> track
+    |> emojify
+    |> format_response
+  end
+
+  def track(%{status: 404, path: path} = conv) do
+    Logger.warn "Warning: #{path} is on the loose!"
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  def rewrite_path(%{path: path} = conv) do
+    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
+  end
+
+  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+    %{ conv | path: "/#{thing}/#{id}" }
+  end
+
+  def rewrite_path_captures(conv, nil), do: conv
+
+  def emojify(%{status: 200} = conv) do
+    emojies = String.duplicate("🎉", 5)
+    body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
+
+    %{ conv | resp_body: body }
+  end
+
+  def emojify(conv), do: conv
+
+  def log(conv), do: IO.inspect conv
+
+  def parse(request) do
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+
+    %{
+      method: method,
+      path: path,
+      resp_body: "",
+      status: nil,
+    }
+  end
+
+  # def route(conv) do
+  #   route(conv, conv.method, conv.path)
+  # end
+
+  def route(%{method: "GET", path: "/wildthings" } = conv) do
+    %{ conv | resp_body: "Bears, Lions, Tigers" }
+  end
+
+  def route(%{ method: "GET", path: "/bears" } = conv) do
+    %{ conv | status: 200, resp_body: "Teddy, Smoky, Paddington" }
+  end
+
+  def route(%{ method: "GET", path: "/bears/new"} = conv) do
+    @pages_path
+      |> Path.join("form.html")
+      |> File.read
+      |> handle_file(conv)
+  end
+
+  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def route(%{method: "DELETE", path: "/bears/" <> id} = conv) do
+    %{ conv | status: 403, resp_body: "Bears must never be deleted! #{id}" }
+  end
+
+  def route(%{ method: "GET", path: "/about" } = conv) do
+    @pages_path
+      |> Path.join("about.html")
+      |> File.read
+      |> handle_file(conv)
+  end
+
+  def route(%{method: "GET", path: "/pages/" <> file} = conv) do
+    @pages_path
+    |> Path.join(file <> ".html")
+    |> File.read
+    |> handle_file(conv)
+  end
+
+  def route(%{path: path} = conv) do
+    %{ conv | status: 404, resp_body: "No #{path} here" }
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content }
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 404, resp_body: "File not found!" }
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason}" }
+  end
+
+  # def route(%{ method: "GET", path: "/about" } = conv) do
+  #   file = Path.expand("../../pages", __DIR__)
+  #     |> Path.join("about.html")
+  #
+  #   case File.read(file) do
+  #     {:ok, content} ->
+  #       %{ conv | status: 200, resp_body: content }
+  #
+  #     {:error, :enoent} ->
+  #       %{ conv | status: 404, resp_body: "File not found!" }
+  #
+  #     {:error, reason} ->
+  #       %{ conv | status: 500, resp_body: "File error: #{reason}" }
+  #   end
+  # end
+
+  def format_response(conv) do
+    # TODO: Use values in the map to create an HTTP response string:
+    """
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    Content-Type: text/html
+    Content-Length: #{byte_size(conv.resp_body)}
+
+    #{conv.resp_body}
+    """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+
+end
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+request = """
+DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+request = """
+GET /bears?id=4 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /lions?id=40 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+request = """
+GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears/new HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /pages/about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+request = """
+GET /pages/test HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
