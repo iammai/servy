4c22bc9001068990fe24c01b65653a28c1ee8457
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-06 21:41:03 -0800

    params no longer hardcoded

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -61,8 +61,7 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{ method: "POST", path: "/bears"} = conv) do
-    params = %{ "name" => "Baloo", "type" => "Brown"}
-    %{ conv | status: 201, resp_body: "Create a #{params["type"]} bear named #{params["name"]}" }
+    %{ conv | status: 201, resp_body: "Create a #{conv.params["type"]} bear named #{conv.params["name"]}" }
   end
 
   def route(%Conv{ method: "GET", path: "/about" } = conv) do
