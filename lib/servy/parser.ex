6562ca0ace0e12faaf725f38fc3dfecfa63dd69b
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-06 22:04:23 -0800

    recursiong head and tail in parser

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -23,8 +23,13 @@ defmodule Servy.Parser do
     }
   end
 
-  def parse_headers(header_lines) do
-    headers = %{}
+  def parse_headers([head | tail]) do
+    IO.puts "Head: #{inspect(head)} Tail#{inspect(tail)}"
+    parse_headers(tail)
+  end
+
+  def parse_headers([]) do
+    IO.puts "Done"
   end
 
   def parse_params(params_string) do
