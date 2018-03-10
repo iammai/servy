f14a9e65cc503345c49c3889e8b3af067baef475
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-10 10:22:45 -0800

    Reset to 14 recursion folder

--- lib/servy/bear_controller.ex
+++
@@ -1,6 +0,0 @@
-defmodule Servy.BearController do
-
-  def index(conv) do
-    %{ conv | status: 200, resp_body: "Teddy, Smoky, Paddington" }
-  end
-end
--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,17 +1,13 @@
-require Logger
-
-alias Servy.Conv
-alias Servy.BearController
-
 defmodule Servy.Handler do
 
   @moduledoc "Handles HTTP requests."
 
-  @pages_path Path.expand("pages", File.cwd!)
+  alias Servy.Conv
+
+  @pages_path Path.expand("../../pages", __DIR__)
 
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
-  import Servy.FileHandler, only: [handle_file: 2]
 
   @doc "Transforms the request into a response."
   def handle(request) do
@@ -21,90 +17,55 @@ defmodule Servy.Handler do
     |> log
     |> route
     |> track
-    |> emojify
     |> format_response
   end
 
-  def emojify(%{status: 200} = conv) do
-    emojies = String.duplicate("🎉", 5)
-    body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
-
-    %{ conv | resp_body: body }
-  end
-
-  def emojify(%Conv{} = conv), do: conv
-
-  # def route(conv) do
-  #   route(conv, conv.method, conv.path)
-  # end
-
   def route(%Conv{ method: "GET", path: "/wildthings" } = conv) do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
   def route(%Conv{ method: "GET", path: "/bears" } = conv) do
-    BearController.index(conv)
-  end
-
-  def route(%Conv{ method: "GET", path: "/bears/new"} = conv) do
-    @pages_path
-      |> Path.join("form.html")
-      |> File.read
-      |> handle_file(conv)
+    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
   def route(%Conv{ method: "GET", path: "/bears/" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 
-  def route(%Conv{ method: "DELETE", path: "/bears/" <> id} = conv) do
-    %{ conv | status: 403, resp_body: "Bears must never be deleted! #{id}" }
+  # name=Baloo&type=Brown
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    %{ conv | status: 201,
+              resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}!" }
   end
 
-  def route(%Conv{ method: "POST", path: "/bears"} = conv) do
-    %{ conv | status: 201, resp_body: "Create a #{conv.params["type"]} bear named #{conv.params["name"]}" }
-  end
-
-  def route(%Conv{ method: "GET", path: "/about" } = conv) do
-    @pages_path
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
+      @pages_path
       |> Path.join("about.html")
       |> File.read
       |> handle_file(conv)
   end
 
-  def route(%Conv{ method: "GET", path: "/pages/" <> file} = conv) do
-    @pages_path
-    |> Path.join(file <> ".html")
-    |> File.read
-    |> handle_file(conv)
+  def route(%Conv{ path: path } = conv) do
+    %{ conv | status: 404, resp_body: "No #{path} here!"}
   end
 
-  def route(%Conv{ path: path} = conv) do
-    %{ conv | status: 404, resp_body: "No #{path} here" }
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content }
   end
 
-  # def route(%{ method: "GET", path: "/about" } = conv) do
-  #   file = Path.expand("../../pages", __DIR__)
-  #     |> Path.join("about.html")
-  #
-  #   case File.read(file) do
-  #     {:ok, content} ->
-  #       %{ conv | status: 200, resp_body: content }
-  #
-  #     {:error, :enoent} ->
-  #       %{ conv | status: 404, resp_body: "File not found!" }
-  #
-  #     {:error, reason} ->
-  #       %{ conv | status: 500, resp_body: "File error: #{reason}" }
-  #   end
-  # end
+  def handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 404, resp_body: "File not found!" }
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason}" }
+  end
 
   def format_response(%Conv{} = conv) do
-    # TODO: Use values in the map to create an HTTP response string:
     """
     HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
-    Content-Length: #{byte_size(conv.resp_body)}
+    Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
@@ -148,7 +109,6 @@ response = Servy.Handler.handle(request)
 
 IO.puts response
 
-
 request = """
 GET /bears/1 HTTP/1.1
 Host: example.com
@@ -161,34 +121,8 @@ response = Servy.Handler.handle(request)
 
 IO.puts response
 
-
 request = """
-DELETE /bears/1 HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-
-request = """
-GET /bears?id=4 HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-request = """
-GET /lions?id=40 HTTP/1.1
+GET /wildlife HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
@@ -213,68 +147,11 @@ response = Servy.Handler.handle(request)
 IO.puts response
 
 request = """
-GET /bears/new HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-request = """
-GET /pages/about HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-
-request = """
-GET /pages/test HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-# POST /bears
-
-request = """
 POST /bears HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
 Content-Type: application/x-www-form-urlencoded
-Content-Length: 21
-
-name=Baloo&type=Brown
-"""
-
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-
-# POST /bears with different Content-Type
-
-request = """
-POST /bears HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-Content-Type: multipart/form-data
 Content-Length: 21
 
 name=Baloo&type=Brown
