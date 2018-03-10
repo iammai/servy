3b2988ab7d46a985d563536623c4528a4171cb46
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-10 13:02:16 -0800

    Get method with params pattern matched

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -30,7 +30,7 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{ method: "GET", path: "/bears/" <> id } = conv) do
-    params = Map.put(conv.param, "id", id)
+    params = Map.put(conv.params, "id", id)
     BearController.show(conv, params)
   end
 
