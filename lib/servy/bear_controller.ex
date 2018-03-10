88af8f734ea2b91c73cc0268d1a8591b14aeb910
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-10 11:00:30 -0800

    Lesson 15 bear controller index and show v1

---
+++ lib/servy/bear_controller.ex
@@ -0,0 +1,11 @@
+defmodule Servy.BearController do
+
+  def index(conv) do
+    %{ conv | status: 200, resp_body: "Teddy, Smoky, Paddington" }
+  end
+
+  def show(conv, id) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+end
