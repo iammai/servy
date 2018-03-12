8dac1064a6b3555bf1a4e66ba45266d7464f0dae
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-11 19:27:15 -0700

    change rest of lines from erlang to elixir

--- lib/servy/http_server.ex
+++ lib/servy/http_server.ex
@@ -9,8 +9,8 @@ server() ->
 def server do
     {:ok, lsock} = :gen_tcp.listen(5678, [:binary, packet: 0,
                                         active: false])
-    {ok, Sock} = gen_tcp:accept(LSock),
-    {ok, Bin} = do_recv(Sock, []),
-    ok = gen_tcp:close(Sock),
-    Bin.
+    {:ok, sock} = :gen_tcp.accept(lsock)
+    {:ok, bin} = :gen_tcp:recv(sock, 0)
+    ok = :gen_tcp.close(sock)
+    bin
 end
