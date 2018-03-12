986c4b60aaf50f525e7e2eb82417459cfc7bb03b
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-11 19:21:13 -0700

    change to def

--- lib/servy/http_server.ex
+++ lib/servy/http_server.ex
@@ -6,10 +6,11 @@ server() ->
     ok = gen_tcp:close(Sock),
     Bin.
 
-server() ->
+def server do
     {ok, LSock} = gen_tcp:listen(5678, [binary, {packet, 0},
                                         {active, false}]),
     {ok, Sock} = gen_tcp:accept(LSock),
     {ok, Bin} = do_recv(Sock, []),
     ok = gen_tcp:close(Sock),
     Bin.
+end
