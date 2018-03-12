b91dcd88346eea1e70b86e762bec9ff55d6427ef
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-11 19:20:54 -0700

    duplicate so we can rewrite it in elixir

--- lib/servy/http_server.ex
+++ lib/servy/http_server.ex
@@ -4,13 +4,12 @@ server() ->
     {ok, Sock} = gen_tcp:accept(LSock),
     {ok, Bin} = do_recv(Sock, []),
     ok = gen_tcp:close(Sock),
-    ok = gen_tcp:close(LSock),
     Bin.
 
-do_recv(Sock, Bs) ->
-    case gen_tcp:recv(Sock, 0) of
-        {ok, B} ->
-            do_recv(Sock, [Bs, B]);
-        {error, closed} ->
-            {ok, list_to_binary(Bs)}
-    end.
+server() ->
+    {ok, LSock} = gen_tcp:listen(5678, [binary, {packet, 0},
+                                        {active, false}]),
+    {ok, Sock} = gen_tcp:accept(LSock),
+    {ok, Bin} = do_recv(Sock, []),
+    ok = gen_tcp:close(Sock),
+    Bin.
