9136ffbbf75e47662a334a64254b78e1e360f447
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-22 18:06:40 -0700

    change bigfoot to Task instead of fetcher - uses task struct

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -24,15 +24,14 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{ method: "GET", path: "/sensors" } = conv) do
-    pid4 = Fetcher.async(fn -> Servy.Tracker.get_location("bigfoot") end)
+    task = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
 
     snapshots =
       ["cam-1", "cam-2", "cam-3"]
       |> Enum.map(&Fetcher.async(fn -> VideoCam.get_snapshot(&1) end))
       |> Enum.map(&Fetcher.get_result/1)
 
-
-    where_is_bigfoot = Fetcher.get_result(pid4)
+    where_is_bigfoot = Task.await(task)
 
     %{ conv | status: 200, resp_body: inspect {snapshots, where_is_bigfoot} }
   end
