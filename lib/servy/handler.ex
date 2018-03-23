442a0ffe47019d84d617597937efc6bb6e02e90a
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-22 18:05:03 -0700

    where is bigfoot spawn to not defend on snapshots getting the result

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -24,14 +24,13 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{ method: "GET", path: "/sensors" } = conv) do
-    snapshots =
-      [
-        Fetcher.async(fn -> VideoCam.get_snapshot("cam-1") end),
-        Fetcher.async(fn -> VideoCam.get_snapshot("cam-2") end),
-        Fetcher.async(fn -> VideoCam.get_snapshot("cam-3") end)
-      ] |> Enum.map(&Fetcher.get_result/1)
-
     pid4 = Fetcher.async(fn -> Servy.Tracker.get_location("bigfoot") end)
+
+    snapshots =
+      ["cam-1", "cam-2", "cam-3"]
+      |> Enum.map(&Fetcher.async(fn -> VideoCam.get_snapshot(&1) end))
+      |> Enum.map(&Fetcher.get_result/1)
+
 
     where_is_bigfoot = Fetcher.get_result(pid4)
 
