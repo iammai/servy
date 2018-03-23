bbd9ad039cc41432459829dd74f8763aea298e06
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-22 21:04:58 -0700

    scaffold listen loop function to store state in processes

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,16 +1,25 @@
 defmodule Servy.PledgeServer do
 
-  def create_pledge(name, amount) do
-    {:ok, id} = send_pledge_to_service(name, amount)
+  def listen_loop do
+    IO.puts"\nWaiting for a message..."
 
-    #Cache the pledge in memory:
-    [ {"larry", 10} ]
+    receive do
+      {:message_type, value} ->
+        $cpde
+    end
   end
 
-  def recent_pledges do
-    # Returns the most recent pledges (cache):
-    [ {"larry", 10} ]
-  end
+  # def create_pledge(name, amount) do
+  #   {:ok, id} = send_pledge_to_service(name, amount)
+  #
+  #   #Cache the pledge in memory:
+  #   [ {"larry", 10} ]
+  # end
+  #
+  # def recent_pledges do
+  #   # Returns the most recent pledges (cache):
+  #   [ {"larry", 10} ]
+  # end
 
   defp send_pledge_to_service(_name, _amount) do
     #CODE HERE TO SEND PLEDGE TO EXTERNAL SERVICE
