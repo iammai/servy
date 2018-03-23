f23daadee15550fd65a51871b3053bb6d7682a2f
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-22 20:58:06 -0700

    Stub out pledge server

---
+++ lib/servy/pledge_server.ex
@@ -0,0 +1,20 @@
+defmodule Servy.PledgeServer do
+
+  def create_pledge(name, amount) do
+    {:ok, id} = send_pledge_to_service(name, amount)
+
+    #Cache the pledge in memory:
+    [ {"larry", 10} ]
+  end
+
+  def recent_pledges do
+    # Returns the most recent pledges (cache):
+    [ {"larry", 10} ]
+  end
+
+  defp send_pledge_to_service(_name, _amount) do
+    #CODE HERE TO SEND PLEDGE TO EXTERNAL SERVICE
+    {:ok, "pledge-#{:rand.uniform(1000)}"}
+  end
+
+end
